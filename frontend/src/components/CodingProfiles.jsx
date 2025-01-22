da74d90f26503e1aee9f250f54e237c7f7799fbb
Vishrut Panya <vishrut.panya@eagleview.com>
2025-01-22 13:18:58 +0530

    codingprofiles.jsx conflict merged

--- frontend/src/components/CodingProfiles.jsx
+++ frontend/src/components/CodingProfiles.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { Box, TextField, Button, Grid2, Typography, List, ListItem, ListItemText, IconButton } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import useResumeStore from '../app/ResumeStore';
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function CodingProfiles() {
   const [currentProfile, setCurrentProfile] = useState({
@@ -23,15 +25,49 @@ export default function CodingProfiles() {
   const handleAddProfile = () => {
     if (currentProfile.platform.trim() !== '' && currentProfile.profileLink.trim() !== '') {
       addCodingProfile(currentProfile);
+      toast.success("Coding Profile added successfully!", {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
       setCurrentProfile({
         platform: '',
         profileLink: '',
+      });
+    }
+
+    else
+    {
+      toast.error("Please enter all details!", {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
       });
     }
   };
 
   const handleDeleteProfile = (index) => {
     deleteCodingProfile(index);
+    toast.success("Coding Profile deleted successfully!", {
+      position: "top-center",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
   };
 
   return (
@@ -43,7 +79,7 @@ export default function CodingProfiles() {
         <Grid2 item xs={12} sm={6}>
           <TextField
             fullWidth
-            label="Platform (e.g., LeetCode, GitHub)"
+            label="Platform (e.g., LeetCode, GitHub)*"
             name="platform"
             value={currentProfile.platform}
             onChange={handleChange}

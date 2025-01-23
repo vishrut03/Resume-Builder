3b17b3b774a251a3ba146ed1cd7bf899af4b2170
Sahil Sidana <sahilsidana2002@gmail.com>
2025-01-23 16:57:25 +0530

    bug in schema

--- frontend/src/schemas/WorkExperienceSchema.js
+++ frontend/src/schemas/WorkExperienceSchema.js
@@ -3,7 +3,7 @@ import * as Yup from 'yup';
 export const WorkExperienceSchema = Yup.object().shape({
     jobTitle: Yup.string().required('Job Title is required'),
     companyName: Yup.string().required('Company Name is required'),
-    startDate: Yup.date().required('Start Date is required').min(new Date(), 'Start Date is invalid'),
+    startDate: Yup.date().required('Start Date is required').max(new Date(), 'Start Date is invalid'),
     // endDate: Yup.date(),
     responsibilities: Yup.string().required('Responsibilities are required'),
 });

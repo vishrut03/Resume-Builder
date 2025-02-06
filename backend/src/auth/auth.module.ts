3a83c4ec7d82644457d695d4534ebb3a099c04e5
Sahil Sidana <sahilsidana2002@gmail.com>
2025-02-06 16:24:27 +0530

    token partial

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,11 +3,20 @@ import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { MongooseModule } from "@nestjs/mongoose";
 import { UserSchema } from "./schemas/user.schema";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
+    imports: [
+        MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+        JwtModule.register({
+            global: true,
+            secret: (process.env.JWT_SECRET || 'defaultSecret'),//process.env not working
+            signOptions: { expiresIn: '60m' }
+        })
+    ],
     controllers: [AuthController],
     providers: [AuthService],
 })
+
 
 export class AuthModule {}
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -4,16 +4,20 @@ import { InjectModel } from "@nestjs/mongoose";
 import { User } from "./schemas/user.schema";
 import * as mongoose from "mongoose";
 import * as bcrypt from 'bcrypt';
+import { JwtService } from "@nestjs/jwt";
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectModel(User.name)
-        private userModel: mongoose.Model<User>
+        private userModel: mongoose.Model<User>,
+        private jwtService: JwtService
     ) {}
 
     async signIn(user: CreateUser) {
         // Check if the user exists
+
+        // console.log(process.env.JWT_SECRET);
         const existingUser = await this.userModel.findOne({ email: user.email });
         if (!existingUser) {
             throw new HttpException('User not found', HttpStatus.NOT_FOUND);
@@ -37,6 +41,9 @@ export class AuthService {
             throw new HttpException('User already exists', HttpStatus.CONFLICT);
         }
 
+
+        //verify user email will come here 
+
         // Hash the password
         const saltOrRounds = 10;
         const hashedPassword = await bcrypt.hash(user.password, saltOrRounds);
@@ -48,8 +55,10 @@ export class AuthService {
         });
         await newUser.save();
 
+        const accessToken = await this.jwtService.signAsync({ email: newUser.email, sub: newUser._id });
+
         // Remove password before returning response
-        const { password, ...userWithoutPassword } = newUser.toObject();
-        return userWithoutPassword;
+        const dbUser = newUser.toObject();
+        return {dbUser, accessToken};
     }
 }

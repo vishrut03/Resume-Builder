fa6b6d66d7584e766a337ff035307ed4df610537
Vishrut Panya <vishrut.panya@eagleview.com>
2025-02-06 18:29:33 +0530

    Linked schema with module file

--- backend/src/resume/resume.module.ts
+++ backend/src/resume/resume.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ResumeController } from './resume.controller';
 import { ResumeService } from './resume.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ResumeSchema } from './schemas/resume.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Resume', schema: ResumeSchema }]),
+  ],
   controllers: [ResumeController],
   providers: [ResumeService],
 })

69eb7213b1b8e3fc1b98c103ad9685233a98958c
Sahil Sidana <sahilsidana2002@gmail.com>
2025-02-07 10:23:31 +0530

    swagger auth

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,16 +2,24 @@ import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger"
 // import * as dotenv from 'dotenv';
-
+import { AuthGuard } from "./auth/guards/auth.guard"
 // dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-
+  
   const config = new DocumentBuilder()
     .setTitle("Resume-Builder")
     .setDescription("The Resume-Builder API description")
     .setVersion("1.0")
+    .addBearerAuth({
+      type: "http",
+      scheme: "bearer",
+      bearerFormat: "JWT",
+      name: "JWT",
+      description: "Enter JWT token",
+      in: "header",
+    }, "JWT-auth")
     .build()
 
   const document = SwaggerModule.createDocument(app, config)
--- backend/src/resume/resume.controller.ts
+++ backend/src/resume/resume.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiSecurity } from '@nestjs/swagger';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
-
 @Controller('resume')
 export class ResumeController {
 
   //'resume/check' ko authenticate krra hai 
   @UseGuards(AuthGuard)
   @Get('check')
-
+  @ApiBearerAuth('JWT-auth')//for swagger lock
   check(@Request() request): string {
     return request.user;
   }
